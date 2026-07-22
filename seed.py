b332a66b072adc23b8cd10b6e4f050c1470a2904
isnufr <isnufatkhul@gmail.com>
2026-07-22 19:01:44 +0700

    Fix: Remove destructive db.drop_all from seed and stop reseeding if DB has users

--- seed.py
+++ seed.py
@@ -6,9 +6,13 @@ from models import User, Kategori, Barang
 
 def seed_data():
     with app.app_context():
-        print("Mereset tabel database...")
-        db.drop_all()
+        print("Memeriksa struktur database...")
         db.create_all()
+
+        # Cek apakah sudah ada admin, jika belum baru jalankan seeder
+        if User.query.filter_by(username='admin').first():
+            print("Database sudah berisi data pengguna, melewati proses reset awal.")
+            return
 
         print("Menambahkan data admin awal...")
         # Tambah Admin
@@ -36,17 +40,6 @@ def seed_data():
         db.session.add_all([kat_sembako, kat_minuman, kat_snack])
         db.session.commit()
 
-        print("Menambahkan barang dummy...")
-        # Sembako
-        b1 = Barang(kode='8999999012345', nama='Minyak Goreng Bimoli 2L', kategori_id=kat_sembako.id, harga_beli=30000, harga_jual=35000, stok=12, stok_min=5)
-        b2 = Barang(kode='BRG0001', nama='Beras Rojolele 5kg', kategori_id=kat_sembako.id, harga_beli=65000, harga_jual=75000, stok=15, stok_min=10)
-        b3 = Barang(kode='8990987654321', nama='Gula Pasir Gulaku 1kg', kategori_id=kat_sembako.id, harga_beli=14000, harga_jual=16500, stok=4, stok_min=10)
-        
-        # Minuman / Snack
-        b4 = Barang(kode='8991234567890', nama='Indomie Goreng', kategori_id=kat_snack.id, harga_beli=2500, harga_jual=3000, stok=45, stok_min=40)
-        b5 = Barang(kode='8993216549870', nama='Teh Celup Sosro', kategori_id=kat_minuman.id, harga_beli=5000, harga_jual=6500, stok=25, stok_min=10)
-        
-        db.session.add_all([b1, b2, b3, b4, b5])
         db.session.commit()
         print("Data awal (seeder) berhasil ditambahkan!")
 
